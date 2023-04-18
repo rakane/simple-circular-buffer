0a74af8c7a21a115b9656e023428fd531a01764e
Ryan Kane <rakane@syr.edu>
2023-04-18 19:15:29 -0400

    Initial put and get implementation

--- __tests__/CircularBuffer.test.ts
+++ __tests__/CircularBuffer.test.ts
@@ -1,5 +1,105 @@
 import { CircularBuffer } from '../src/index';
 
-test('CircularBuffer', () => {
-  expect(true).toBe(true);
+test('CircularBuffer - Overwrite not allowed', () => {
+  const buffer = new CircularBuffer(5, false);
+  expect(buffer.capacity).toBe(5);
+  expect(buffer.size).toBe(0);
+  expect(buffer.allowOverwrite).toBe(false);
+
+  // Fill the buffer
+  expect(buffer.put(1)).toBe(true);
+  expect(buffer.put(2)).toBe(true);
+  expect(buffer.put(3)).toBe(true);
+  expect(buffer.put(4)).toBe(true);
+  expect(buffer.put(5)).toBe(true);
+
+  expect(buffer.size).toBe(5);
+  expect(buffer._readIndex).toBe(0);
+  expect(buffer._writeIndex).toBe(0);
+
+  // Read the buffer
+  expect(buffer.get()).toBe(1);
+  expect(buffer.get()).toBe(2);
+  expect(buffer.get()).toBe(3);
+  expect(buffer.get()).toBe(4);
+  expect(buffer.get()).toBe(5);
+
+  expect(buffer.size).toBe(0);
+  expect(buffer._readIndex).toBe(0);
+  expect(buffer._writeIndex).toBe(0);
+
+  // Fill the buffer again
+  expect(buffer.put(6)).toBe(true);
+  expect(buffer.put(7)).toBe(true);
+  expect(buffer.put(8)).toBe(true);
+  expect(buffer.put(9)).toBe(true);
+  expect(buffer.put(10)).toBe(true);
+  expect(buffer.put(11)).toBe(false);
+
+  expect(buffer.size).toBe(5);
+  expect(buffer._readIndex).toBe(0);
+  expect(buffer._writeIndex).toBe(0);
+
+  // Read the buffer again (6 should be overwritten)
+  expect(buffer.get()).toBe(6);
+  expect(buffer.get()).toBe(7);
+  expect(buffer.get()).toBe(8);
+  expect(buffer.get()).toBe(9);
+  expect(buffer.get()).toBe(10);
+
+  expect(buffer.size).toBe(0);
+  expect(buffer._readIndex).toBe(0);
+  expect(buffer._writeIndex).toBe(0);
+});
+
+test('CircularBuffer - Overwrite allowed', () => {
+  const buffer = new CircularBuffer(5, true);
+  expect(buffer.capacity).toBe(5);
+  expect(buffer.size).toBe(0);
+  expect(buffer.allowOverwrite).toBe(true);
+
+  // Fill the buffer
+  expect(buffer.put(1)).toBe(true);
+  expect(buffer.put(2)).toBe(true);
+  expect(buffer.put(3)).toBe(true);
+  expect(buffer.put(4)).toBe(true);
+  expect(buffer.put(5)).toBe(true);
+
+  expect(buffer.size).toBe(5);
+  expect(buffer._readIndex).toBe(0);
+  expect(buffer._writeIndex).toBe(0);
+
+  // Read the buffer
+  expect(buffer.get()).toBe(1);
+  expect(buffer.get()).toBe(2);
+  expect(buffer.get()).toBe(3);
+  expect(buffer.get()).toBe(4);
+  expect(buffer.get()).toBe(5);
+
+  expect(buffer.size).toBe(0);
+  expect(buffer._readIndex).toBe(0);
+  expect(buffer._writeIndex).toBe(0);
+
+  // Fill the buffer again
+  expect(buffer.put(6)).toBe(true);
+  expect(buffer.put(7)).toBe(true);
+  expect(buffer.put(8)).toBe(true);
+  expect(buffer.put(9)).toBe(true);
+  expect(buffer.put(10)).toBe(true);
+  expect(buffer.put(11)).toBe(true);
+
+  expect(buffer.size).toBe(5);
+  expect(buffer._readIndex).toBe(0);
+  expect(buffer._writeIndex).toBe(1);
+
+  // Read the buffer again (6 should be overwritten)
+  expect(buffer.get()).toBe(11);
+  expect(buffer.get()).toBe(7);
+  expect(buffer.get()).toBe(8);
+  expect(buffer.get()).toBe(9);
+  expect(buffer.get()).toBe(10);
+
+  expect(buffer.size).toBe(0);
+  expect(buffer._readIndex).toBe(0);
+  expect(buffer._writeIndex).toBe(1);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,80 @@
-export class CircularBuffer {
-  constructor() {}
+export class CircularBuffer<T> {
+  // The maximum number of items the buffer can hold
+  _capacity: number;
+
+  // The current number of items in the buffer
+  _size: number;
+
+  // The buffer
+  _buffer: (T | null)[];
+
+  // The index of the next item to be read
+  _readIndex: number;
+
+  // The index of the next item to be written
+  _writeIndex: number;
+
+  // Allow overwriting of items in the buffer
+  _allowOverwrite: boolean;
+
+  constructor(size: number, allowOverwrite: boolean = false) {
+    // Initialize sizes and buffer
+    this._capacity = size;
+    this._size = 0;
+    this._buffer = new Array<T | null>(size);
+
+    for (let i = 0; i < size; i++) {
+      this._buffer[i] = null;
+    }
+
+    // Set the read and write indices
+    this._readIndex = 0;
+    this._writeIndex = 0;
+
+    // Set the overwrite flag
+    this._allowOverwrite = allowOverwrite;
+  }
+
+  get capacity() {
+    return this._capacity;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  get allowOverwrite() {
+    return this._allowOverwrite;
+  }
+
+  put(item: T): boolean {
+    const canWrite = this._allowOverwrite || this._size < this._capacity;
+
+    if (canWrite) {
+      this._buffer[this._writeIndex++] = item;
+      this._writeIndex = this._writeIndex % this._capacity;
+
+      // Increase the size if we're not overwriting
+      if (this._size < this._capacity) {
+        this._size++;
+      }
+    }
+
+    return canWrite;
+  }
+
+  get(): T | null {
+    if (this._size === 0) return null;
+
+    const item = this._buffer[this._readIndex++];
+
+    this._readIndex = this._readIndex % this._capacity;
+    this._size--;
+
+    return item;
+  }
+
+  print() {
+    console.log(this._buffer);
+  }
 }
